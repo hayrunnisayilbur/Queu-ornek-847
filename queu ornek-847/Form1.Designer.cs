5f2627d819c692f6bb98da2e1b70bbecd07f928e
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2024-02-06 12:19:12 +0300

    Add files via upload

---
+++ queu ornek-847/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace queu_ornek_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnCagir = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnCagir1 = new System.Windows.Forms.Button();
+            this.btnCagir2 = new System.Windows.Forms.Button();
+            this.btnCagir3 = new System.Windows.Forms.Button();
+            this.lblCagir2 = new System.Windows.Forms.Label();
+            this.lblCagir3 = new System.Windows.Forms.Label();
+            this.lblCagir4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(51, 311);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(56, 204);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(161, 31);
+            this.txtAd.TabIndex = 3;
+            // 
+            // btnCagir
+            // 
+            this.btnCagir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCagir.Location = new System.Drawing.Point(243, 257);
+            this.btnCagir.Name = "btnCagir";
+            this.btnCagir.Size = new System.Drawing.Size(152, 33);
+            this.btnCagir.TabIndex = 4;
+            this.btnCagir.Text = "çağır";
+            this.btnCagir.UseVisualStyleBackColor = true;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(56, 257);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(161, 33);
+            this.btnEkle.TabIndex = 5;
+            this.btnEkle.Text = "müşteri ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnCagir1
+            // 
+            this.btnCagir1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCagir1.Location = new System.Drawing.Point(52, 41);
+            this.btnCagir1.Name = "btnCagir1";
+            this.btnCagir1.Size = new System.Drawing.Size(125, 38);
+            this.btnCagir1.TabIndex = 6;
+            this.btnCagir1.Text = "çağır1";
+            this.btnCagir1.UseVisualStyleBackColor = true;
+            this.btnCagir1.Click += new System.EventHandler(this.btnCagir1_Click);
+            // 
+            // btnCagir2
+            // 
+            this.btnCagir2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCagir2.Location = new System.Drawing.Point(251, 40);
+            this.btnCagir2.Name = "btnCagir2";
+            this.btnCagir2.Size = new System.Drawing.Size(122, 39);
+            this.btnCagir2.TabIndex = 7;
+            this.btnCagir2.Text = "çağır2";
+            this.btnCagir2.UseVisualStyleBackColor = true;
+            this.btnCagir2.Click += new System.EventHandler(this.btnCagir2_Click);
+            // 
+            // btnCagir3
+            // 
+            this.btnCagir3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCagir3.Location = new System.Drawing.Point(468, 40);
+            this.btnCagir3.Name = "btnCagir3";
+            this.btnCagir3.Size = new System.Drawing.Size(110, 39);
+            this.btnCagir3.TabIndex = 8;
+            this.btnCagir3.Text = "çağır3";
+            this.btnCagir3.UseVisualStyleBackColor = true;
+            this.btnCagir3.Click += new System.EventHandler(this.btnCagir3_Click);
+            // 
+            // lblCagir2
+            // 
+            this.lblCagir2.AutoSize = true;
+            this.lblCagir2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCagir2.Location = new System.Drawing.Point(52, 99);
+            this.lblCagir2.Name = "lblCagir2";
+            this.lblCagir2.Size = new System.Drawing.Size(60, 24);
+            this.lblCagir2.TabIndex = 9;
+            this.lblCagir2.Text = "label2";
+            // 
+            // lblCagir3
+            // 
+            this.lblCagir3.AutoSize = true;
+            this.lblCagir3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCagir3.Location = new System.Drawing.Point(251, 99);
+            this.lblCagir3.Name = "lblCagir3";
+            this.lblCagir3.Size = new System.Drawing.Size(60, 24);
+            this.lblCagir3.TabIndex = 10;
+            this.lblCagir3.Text = "label3";
+            // 
+            // lblCagir4
+            // 
+            this.lblCagir4.AutoSize = true;
+            this.lblCagir4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCagir4.Location = new System.Drawing.Point(482, 99);
+            this.lblCagir4.Name = "lblCagir4";
+            this.lblCagir4.Size = new System.Drawing.Size(60, 24);
+            this.lblCagir4.TabIndex = 11;
+            this.lblCagir4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCagir4);
+            this.Controls.Add(this.lblCagir3);
+            this.Controls.Add(this.lblCagir2);
+            this.Controls.Add(this.btnCagir3);
+            this.Controls.Add(this.btnCagir2);
+            this.Controls.Add(this.btnCagir1);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnCagir);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnCagir;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnCagir1;
+        private System.Windows.Forms.Button btnCagir2;
+        private System.Windows.Forms.Button btnCagir3;
+        private System.Windows.Forms.Label lblCagir2;
+        private System.Windows.Forms.Label lblCagir3;
+        private System.Windows.Forms.Label lblCagir4;
+    }
+}
+
---
+++ queu ornek-847/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace queu_ornek_847
+{
+    public partial class Form1 : Form
+    {
+        Stack< string>musteri=new Stack< string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            musteri.Push(txtAd.Text);
+        }
+
+         private void musteriCagir(Label lbl)
+        {
+            if (musteri.Count > 0)
+            {
+                label1.Text = musteri.Pop();
+            }
+            else
+            {
+                MessageBox.Show("işini hızlı bitirdin...");
+            }
+        }
+            
+
+private void Form1_Load(object sender, EventArgs e)
+        {
+            musteri.Push("hayrunnisa");
+            musteri.Push("kübra");
+            musteri.Push("sukeyna");
+            musteri.Push("rabia");
+
+        }
+
+        private void btnCagir1_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblCagir2);
+        }
+
+        private void btnCagir2_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblCagir3);
+
+        }
+
+        private void btnCagir3_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblCagir4);
+
+        }
+    }
+}
